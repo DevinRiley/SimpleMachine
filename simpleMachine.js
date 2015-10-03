838bdb649170e2f56c525308d3625ad3faf94fc3
devin.riley@getbraintree.com <code@getbraintree.com>
2015-10-02 17:36:57 -0700

    WIP

--- simpleMachine.js
+++ simpleMachine.js
@@ -1,7 +1,14 @@
 var SimpleMachine = {
-  accumulator: 0;
+  accumulator: 0,
+  addressRegister: 0,
+  memoryAddressReigster: 0,
+  memoryBufferRegister: 0,
+  instructionRegister: 0,
+  programCounter: 0,
+  counter: 0,
+  memory: new Array(10000),
 };
-// should have registers
-// should have memory
+// memory should be loaded with program contents (strip out comments and blank lines first)
+// there should be something resembling the fetch/decode/execute cycle
 // should interpet instructions
 // should have datapaths implicit in
--- test/simpleMachineSpec.js
+++ test/simpleMachineSpec.js
@@ -3,7 +3,18 @@ describe("SimpleMachine", function() {
         expect(SimpleMachine).toEqual(jasmine.anything());
     });
 
-    it("should have an accumulator register that returns a number", function() {
-        expect(SimpleMachine.accumulator).toBe(jasmine.any(Number));
+    it("should have the PC, accumulator, counter and all the registers initiliazed to zero", function() {
+        expect(SimpleMachine.accumulator).toBe(0);
+        expect(SimpleMachine.addressRegister).toBe(0);
+        expect(SimpleMachine.memoryAddressReigster).toBe(0);
+        expect(SimpleMachine.memoryBufferRegister).toBe(0);
+        expect(SimpleMachine.instructionRegister).toBe(0);
+        expect(SimpleMachine.programCounter).toBe(0);
+        expect(SimpleMachine.counter).toBe(0);
     });
+
+   it("has 10,000 units of memory", function() {
+     expect(SimpleMachine.memory.length).toBe(10000);
+   });
+
 });
