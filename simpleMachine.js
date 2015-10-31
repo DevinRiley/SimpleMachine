135c617b19c9609f3a6798f7f5f42096556de994
devin.riley@getbraintree.com <code@getbraintree.com>
2015-10-30 18:52:46 -0700

    Add instruction methods and single instruction parsing

--- simpleMachine.js
+++ simpleMachine.js
@@ -1,14 +1,140 @@
 var SimpleMachine = {
   accumulator: 0,
   addressRegister: 0,
-  memoryAddressReigster: 0,
+  memoryAddressRegister: 0,
   memoryBufferRegister: 0,
   instructionRegister: 0,
   programCounter: 0,
   counter: 0,
-  memory: new Array(10000),
+  memory: [],
+
+  OPCODE_MASK:  0b1111000000000000,
+  OPERAND_MASK: 0b0000001111111111,
+
+
+  resetRegisters: function() {
+    this.accumulator =  0;
+    this.addressRegister = 0;
+    this.memoryAddressReigster = 0;
+    this.memoryBufferRegister = 0;
+    this.instructionRegister = 0;
+    this.programCounter = 0;
+    this.counter = 0;
+  },
+
+  execute: function(instructionAsString) {
+    var instruction, functionToExecute;
+
+    instruction = this._parseInstruction(instructionAsString);
+    functionToExecute = this._opcodeToFunction(instruction.opcode)
+    functionToExecute.apply(this, [instruction.operand]);
+  },
+
+  add: function() {
+    this.accumulator += this.memoryBufferRegister;
+  },
+
+  sub: function() {
+    this.accumulator -= this.memoryBufferRegister;
+  },
+
+  addc: function() {
+    this.accumulator += this.counter;
+  },
+
+  mvac: function() {
+    this.counter = this.accumulator;
+  },
+
+  dec: function() {
+    this.counter -= 1;
+  },
+
+  load: function(address) {
+    this.memoryAddressRegister = address;
+    this._get()
+    this.accumulator = this.memoryBufferRegister;
+  },
+
+  store: function(address) {
+    this.memoryAddressRegister = address;
+    this._put()
+  },
+
+  jeq: function(address) {
+    if (this.counter == 0) { this.programCounter = address };
+  },
+
+  jlt: function(address) {
+    if (this.counter < 0) { this.programCounter = address };
+  },
+
+  la: function(address) {
+    this.accumulator = address;
+  },
+
+  lia: function(address) {
+    this.accumulator = this._fetchValueFromMemory(this.addressRegister);
+  },
+
+  sia: function() {
+    this.memory[this.addressRegister] = this._valueToWord(this.accumulator);
+  },
+
+  mvaa: function() {
+    this.addressRegister = this.accumulator;
+  },
+
+  _get: function() {
+    this.memoryBufferRegister = this._fetchValueFromMemory(this.memoryAddressRegister);
+  },
+
+  _put: function() {
+    this.memory[this.memoryAddressRegister] = this._valueToWord(this.memoryBufferRegister);
+  },
+
+  _fetchValueFromMemory: function(address) {
+    return parseInt(this.memory[address]);
+  },
+
+  _valueToWord: function(value) {
+    var hex = value.toString(16).toUpperCase();
+    while (hex.length < 4) { hex = "0" + hex; }
+    return "0x" + hex
+  },
+
+  _parseInstruction: function(instructionAsString) {
+    instruction = parseInt(instructionAsString);
+    if (instruction == NaN) throw "Invalid instruction (must be hex string e.g. 0x8001)";
+    return {
+      opcode:  (instruction & this.OPCODE_MASK) >>> 12,
+      operand: instruction & this.OPERAND_MASK,
+    }
+  },
+
+  _opcodeToFunction: function(opcode) {
+    var cpu = this;
+    var opcodeMap = {
+      0:  cpu.halt,
+      1:  cpu.load,
+      2:  cpu.store,
+      3:  cpu.addc,
+      4:  cpu.mvac,
+      5:  cpu.jeq,
+      6:  cpu.jlt,
+      7:  cpu.jmp,
+      8:  cpu.add,
+      9:  cpu.sub,
+      10: cpu.dec,
+      11: cpu.la,
+      12: cpu.lia,
+      13: cpu.sia,
+      14: cpu.mvaa,
+    }
+
+    return opcodeMap[opcode];
+  },
 };
 // memory should be loaded with program contents (strip out comments and blank lines first)
 // there should be something resembling the fetch/decode/execute cycle
-// should interpet instructions
-// should have datapaths implicit in
+// should have datapaths implicit in how methods work
--- test/simpleMachineSpec.js
+++ test/simpleMachineSpec.js
@@ -6,15 +6,203 @@ describe("SimpleMachine", function() {
     it("should have the PC, accumulator, counter and all the registers initiliazed to zero", function() {
         expect(SimpleMachine.accumulator).toBe(0);
         expect(SimpleMachine.addressRegister).toBe(0);
-        expect(SimpleMachine.memoryAddressReigster).toBe(0);
+        expect(SimpleMachine.memoryAddressRegister).toBe(0);
         expect(SimpleMachine.memoryBufferRegister).toBe(0);
         expect(SimpleMachine.instructionRegister).toBe(0);
         expect(SimpleMachine.programCounter).toBe(0);
         expect(SimpleMachine.counter).toBe(0);
     });
 
-   it("has 10,000 units of memory", function() {
-     expect(SimpleMachine.memory.length).toBe(10000);
-   });
+    it("initializes memory to an empty array", function() {
+      expect(SimpleMachine.memory).toEqual([]);
+    });
 
+    describe(".reset()", function() {
+      it("sets all values to 0", function() {
+        SimpleMachine.accumulator = 1;
+        SimpleMachine.addressRegister = 1;
+        SimpleMachine.memoryBufferRegister = 1;
+        SimpleMachine.instructionRegister = 1;
+        SimpleMachine.programCounter = 1;
+        SimpleMachine.counter = 1;
+
+        SimpleMachine.resetRegisters();
+        expect(SimpleMachine.accumulator).toBe(0);
+        expect(SimpleMachine.addressRegister).toBe(0);
+        expect(SimpleMachine.memoryAddressRegister).toBe(0);
+        expect(SimpleMachine.memoryBufferRegister).toBe(0);
+        expect(SimpleMachine.instructionRegister).toBe(0);
+        expect(SimpleMachine.programCounter).toBe(0);
+        expect(SimpleMachine.counter).toBe(0);
+      });
+    });
+
+    describe("._get()", function() {
+      it("sets the memoryBufferRegister to the contents of memory from memory address register", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.memory = ['0x1234'];
+
+        SimpleMachine._get();
+        expect(SimpleMachine.memoryBufferRegister).toBe(parseInt('0x1234'));
+      });
+    });
+
+    describe("LOAD instruction", function() {
+      it("loads accumulator from memory", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.memory = ['0x0007'];
+
+        SimpleMachine.execute("0x1000");
+        expect(SimpleMachine.accumulator).toBe(7);
+      });
+    });
+
+    describe("STORE instruction", function() {
+      it("stores accumulator to memory", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.memoryBufferRegister = 64;
+
+        SimpleMachine.execute("0x2000");
+        expect(SimpleMachine.memory[0]).toBe('0x0040');
+      });
+    });
+
+    describe("ADD instruction", function() {
+      it("adds memory buffer register to accumulator", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.memoryBufferRegister = 1;
+
+        SimpleMachine.execute("0x8000");
+        expect(SimpleMachine.accumulator).toBe(1);
+      });
+    });
+
+    describe("SUB instruction", function() {
+      it("subtracts memory buffer register from accumulator", function() {
+        SimpleMachine.resetRegisters();
+
+        SimpleMachine.memoryBufferRegister = 1;
+        SimpleMachine.execute("0x9000");
+        expect(SimpleMachine.accumulator).toBe(-1);
+      });
+    });
+
+    describe("ADDC instruction", function() {
+      it("adds the counter to the accumulator", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = 1;
+        SimpleMachine.accumulator = 1;
+
+        SimpleMachine.execute("0x3000");
+        expect(SimpleMachine.accumulator).toBe(2);
+      });
+    });
+
+    describe("MVAC instruction", function() {
+      it("moves the accumulator to the counter", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.accumulator = 7;
+
+        SimpleMachine.execute("0x4000");
+        expect(SimpleMachine.counter).toBe(7);
+      });
+    });
+
+    describe("DEC instruction", function() {
+      it("decrements the counter", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = 7;
+
+        SimpleMachine.execute("0xA000");
+        expect(SimpleMachine.counter).toBe(6);
+      });
+    });
+
+    describe("JEQ instruction", function() {
+      it("changes the PC value if counter is zero", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = 0;
+
+        SimpleMachine.execute("0x500A");
+        expect(SimpleMachine.programCounter).toBe(10);
+      });
+
+      it("doesn't change the PC value if counter is not zero", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = 1;
+
+        SimpleMachine.execute("0x500A");
+        expect(SimpleMachine.programCounter).toBe(0);
+      });
+    });
+
+    describe("JLT instruction", function() {
+      it("changes the PC value if counter is less than zero", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = -1;
+
+        SimpleMachine.execute("0x600A");
+        expect(SimpleMachine.programCounter).toBe(10);
+      });
+
+      it("doesn't change the PC value if counter >= zero", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = 0;
+
+        SimpleMachine.execute("0x600A");
+        expect(SimpleMachine.programCounter).toBe(0);
+      });
+    });
+
+    describe("JMP instruction", function() {
+      it("changes the PC value to the value of the operand", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.counter = -1;
+
+        SimpleMachine.execute("0x600A");
+        expect(SimpleMachine.programCounter).toBe(10);
+      });
+    });
+
+    describe("LA instruction", function() {
+      it("changes the accumulator value to the value of the operand", function() {
+        SimpleMachine.resetRegisters();
+
+        SimpleMachine.execute("0xBFFF");
+        expect(SimpleMachine.accumulator).toBe(1023);
+      });
+    });
+
+    describe("LIA instruction", function() {
+      it("loads accumulator with value from address in the areg", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.memory = ["0x0000", "0x000A"];
+        SimpleMachine.addressRegister = 1;
+
+        SimpleMachine.execute("0xC000");
+        expect(SimpleMachine.accumulator).toBe(10);
+      });
+    });
+
+    describe("SIA instruction", function() {
+      it("stores the accumulator value at address in areg", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.accumulator = 17;
+        SimpleMachine.addressRegister = 0;
+        SimpleMachine.memory = ["0x0000", "0x000A"];
+
+        SimpleMachine.execute("0xD000");
+        expect(SimpleMachine.memory[0]).toBe("0x0011");
+      });
+    });
+
+    describe("MVAA instruction", function() {
+      it("moves the value in the accumulator to the areg", function() {
+        SimpleMachine.resetRegisters();
+        SimpleMachine.accumulator = 17;
+
+        SimpleMachine.execute("0xE000");
+        expect(SimpleMachine.addressRegister).toBe(17);
+      });
+    });
 });
